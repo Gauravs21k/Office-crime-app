27f6afcf23c48c880843c2d6ffc1b34d28fba448
Gaurav Singh <gauravs21k@gmail.com>
2022-02-08 00:52:54 +0530

    add new icon added in actionbar

--- app/src/main/java/com/gaurav/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/gaurav/criminalintent/CrimeListFragment.java
@@ -1,20 +1,18 @@
 package com.gaurav.criminalintent;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.text.DateFormat;
@@ -24,6 +22,18 @@ public class CrimeListFragment extends Fragment {
     private RecyclerView cCrimeRecyclerView;
     private CrimeAdapter cAdapter;
     private int position;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_crime_list,menu);
+    }
 
     @Nullable
     @Override
